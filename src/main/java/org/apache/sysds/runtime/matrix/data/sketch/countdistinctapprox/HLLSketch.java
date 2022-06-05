0bc00b68c3c6a5fb62b30057e4a01dbd94d3eb86
Badrul Chowdhury <badrulchowdhury17@gmail.com>
2022-06-05 15:01:46 -0700

    [WIP] Implement LogLog, SuperLogLog, HyperLogLog for CP RowCol

--- src/main/java/org/apache/sysds/runtime/matrix/data/sketch/countdistinctapprox/HLLSketch.java
+++ src/main/java/org/apache/sysds/runtime/matrix/data/sketch/countdistinctapprox/HLLSketch.java
@@ -107,11 +107,14 @@ public class HLLSketch extends CountDistinctApproxSketch {
          */
 
         LogLogHashBucket hashBuckets = new LogLogHashBucket(K);
-        int R = blkIn.getNumRows(), C = blkIn.getNumColumns();
-        if (this.op.getDirection() == Types.Direction.RowCol) {
-            int N = (int)Math.pow(2, K) + 1;
+        int R = blkIn.getNumRows();
+        int C = blkIn.getNumColumns();
 
-            MatrixBlock blkOutCorr = new MatrixBlock(2, N, false);
+        if (this.op.getDirection() == Types.Direction.RowCol) {
+            // Todo We need a better name for N
+            int maxNumHashes = (int)Math.pow(2, K) + 1;
+
+            MatrixBlock blkOutCorr = new MatrixBlock(2, maxNumHashes, false);
             // Push all values into the hash bucket data structure
             for (int i=0; i<R; ++i) {
                 for (int j=0; j<C; ++j) {
@@ -132,16 +135,18 @@ public class HLLSketch extends CountDistinctApproxSketch {
                 ++j;
             }
 
-            blkOutCorr.setValue(0, N-1, hashBuckets.size());
-            blkOutCorr.setValue(1, N-1, hashBuckets.size());
+            // The exact number of hashes is stored in the last column of
+            // the correction matrix in the RowCol case
+            blkOutCorr.setValue(0, maxNumHashes-1, hashBuckets.size());
+            blkOutCorr.setValue(1, maxNumHashes-1, hashBuckets.size());
 
             return new CorrMatrixBlock(null, blkOutCorr);
 
         } else if (this.op.getDirection() == Types.Direction.Row) {
-            int N = C + 1;
+            int maxNumHashes = C + 1;
 
             MatrixBlock blkOut = blkIn;
-            MatrixBlock blkOutCorr = new MatrixBlock(R, N, false);
+            MatrixBlock blkOutCorr = new MatrixBlock(R, maxNumHashes, false);
 
             for (int i=0; i<R; ++i) {
                 for (int j=0; j<C; ++j) {
@@ -160,7 +165,10 @@ public class HLLSketch extends CountDistinctApproxSketch {
                     blkOutCorr.setValue(i, j, v);
                     ++j;
                 }
-                blkOutCorr.setValue(i, N-1, hashBuckets.size());
+
+                // The exact number of hashes is stored in the last column of
+                // each row in the correction matrix
+                blkOutCorr.setValue(i, maxNumHashes-1, hashBuckets.size());
 
                 // Prepare hash buckets data structure for new row
                 hashBuckets.clear();
@@ -191,6 +199,9 @@ public class HLLSketch extends CountDistinctApproxSketch {
                     blkOutCorr.setValue(i, j, v);
                     ++i;
                 }
+
+                // The exact number of hashes is stored in the last row of
+                // each column in the correction matrix
                 blkOutCorr.setValue(N-1, j, hashBuckets.size());
 
                 // Prepare hash buckets data structure for new col
@@ -210,6 +221,8 @@ public class HLLSketch extends CountDistinctApproxSketch {
         // Todo we have to be careful when taking the union in the row/col cases:
         //   we have to store the union in the larger of the 2 and ensure that
         //   reduceByKey() always retains the larger of the 2
+        //   -> will swapping at the beginning of each case work?
+        //   Swap to ensure the left matrix is always the larger of the 2
         MatrixBlock blkInA = arg0.getValue();
         MatrixBlock blkInCorrA = arg0.getCorrection();
 
@@ -254,7 +267,7 @@ public class HLLSketch extends CountDistinctApproxSketch {
                 int v = kvPair.getValue();
 
                 blkOutCorr.setValue(0, j, k);
-                blkOutCorr.setValue(1, j, k);
+                blkOutCorr.setValue(1, j, v);
                 ++j;
             }
 
@@ -265,28 +278,183 @@ public class HLLSketch extends CountDistinctApproxSketch {
 
         } else if (this.op.getDirection() == Types.Direction.Row) {
 
-            int nA = (int)blkInCorrA.getValue(0, blkInCorrA.getNumColumns() - 1);
-            int nB = (int)blkInCorrB.getValue(0, blkInCorrB.getNumColumns() - 1);
-
-            MatrixBlock blkOut = blkInA;
-            MatrixBlock blkOutCorr = blkInCorrA;
-            if (nB > nA) {
-                blkOut = blkInB;
-                blkOutCorr = blkInCorrB;
-            }
+            // Todo Proposed optimization: unroll loop (?) and iterate over 2 sketches simultaneously
+            // [P] | [Q]
+            // [R] | [S]
+            // Assume [P] is overlapping sub-matrix.
+            // The proposed optimization strategy:
+            // 1. Iterate through both sketches simultaneously for [P]
+            // 2. Iterate through larger sketch only for [Q], [R], and [S]
+            //
+            // It may be easier to iterate through [Q] + [S] and [R] + [S].
+            // Double iteration over [S] will not affect correctness as the hash bucket
+            // data structure retains the largest key anyway.
+            //
+            // It would affect performance though:
+            // The difference in size between [P] and [S] will affect performance most because
+            // the main objective of this optimization is to prevent iterating over 2 sub-matrices
+            // of size [P] twice in series for the input matrices separately.
 
             LogLogHashBucket hashBuckets = new LogLogHashBucket(K);
 
-            // Todo implement
-            return new CorrMatrixBlock(null, blkOutCorr);
+            MatrixBlock tallerMatrix = blkInA;
+            MatrixBlock tallerMatrixCorr = blkInCorrA;
+            int largerR = blkInA.getNumRows();
+            int smallerR = blkInB.getNumRows();
+            if (blkInA.getNumRows() < blkInB.getNumRows()) {
+                tallerMatrix = blkInB;
+                tallerMatrixCorr = blkInCorrB;
+                largerR = blkInB.getNumRows();
+                smallerR = blkInA.getNumRows();
+            }
+
+            // Allocate new memory: Todo rationale
+            MatrixBlock blkOut = new MatrixBlock(Math.max(blkInA.getNumRows(), blkInB.getNumRows()), Math.max(blkInA.getNumColumns(), blkInB.getNumColumns()), false);
+            MatrixBlock blkOutCorr = new MatrixBlock(Math.max(blkInA.getNumRows(), blkInB.getNumRows()), Math.max(blkInA.getNumColumns(), blkInB.getNumColumns()) + 1, false);
+
+            for (int i=0; i<smallerR; ++i) {
+                int nA = (int)blkInCorrA.getValue(i, blkInCorrA.getNumColumns() - 1);
+                int nB = (int)blkInCorrB.getValue(i, blkInCorrB.getNumColumns() - 1);
+
+                MatrixBlock narrowerMatrix = blkInA;
+                MatrixBlock narrowerMatrixCorr = blkInCorrA;
+                int smallerC = blkInA.getNumColumns();
+                int largerC = blkInB.getNumColumns();
+                if (nB < nA) {
+                    narrowerMatrix = blkInB;
+                    narrowerMatrixCorr = blkInCorrB;
+                    smallerC = blkInB.getNumColumns();
+                    largerC = blkInA.getNumColumns();
+                }
+
+                for (int j=0; j<smallerC; ++j) {
+                    int k = (int)blkInA.getValue(i, j);
+                    int v = (int)blkInCorrA.getValue(i, j);
+                    hashBuckets.put(k, v);
+
+                    k = (int)blkInB.getValue(i, j);
+                    v = (int)blkInCorrB.getValue(i, j);
+                    hashBuckets.put(k, v);
+                }
+
+                // Todo fix bug: need wider matrix here instead of narrower
+                for (int j=smallerC; j<largerC; ++j) {
+                    int k = (int)narrowerMatrix.getValue(i, j);
+                    int v = (int)narrowerMatrixCorr.getValue(i, j);
+                    hashBuckets.put(k, v);
+                }
+
+                // Serialize hash bucket for this row to output matrix
+                int j=0;
+                for (Pair<Integer, Integer> kvPair : hashBuckets) {
+                    int k = kvPair.getKey();
+                    int v = kvPair.getValue();
+                    blkOut.setValue(i, j, k);
+                    blkOutCorr.setValue(i, j, v);
+                }
+
+                blkOutCorr.setValue(i, blkOutCorr.getNumColumns() - 1, hashBuckets.size());
+
+                hashBuckets.clear();
+            }
+
+            // Copy out the taller matrix verbatim - no need for comparison
+            for (int i=smallerR; i<largerR; ++i) {
+                int nHashes = (int)tallerMatrix.getValue(i, tallerMatrix.getNumColumns() - 1);
+                for (int j=0; j<nHashes; ++j) {
+                    int k = (int)tallerMatrix.getValue(i, j);
+                    int v = (int)tallerMatrixCorr.getValue(i, j);
+
+                    blkOut.setValue(i, j, k);
+                    blkOutCorr.setValue(i, j, v);
+                }
+
+                blkInCorrA.setValue(i, blkInCorrA.getNumColumns() - 1, nHashes);
+            }
+
+            return new CorrMatrixBlock(blkOut, blkOutCorr);
 
         } else if (this.op.getDirection() == Types.Direction.Col) {
+
+            LogLogHashBucket hashBuckets = new LogLogHashBucket(K);
+
+            MatrixBlock widerMatrix = blkInA;
+            MatrixBlock widerMatrixCorr = blkInCorrA;
+            int largerC = blkInA.getNumColumns();
+            int smallerC = blkInB.getNumColumns();
+            if (blkInA.getNumColumns() < blkInB.getNumColumns()) {
+                widerMatrix = blkInB;
+                widerMatrixCorr = blkInCorrB;
+                largerC = blkInB.getNumColumns();
+                smallerC = blkInA.getNumColumns();
+            }
+
+            MatrixBlock blkOut = new MatrixBlock(Math.max(blkInA.getNumRows(), blkInB.getNumRows()), Math.max(blkInA.getNumColumns(), blkInB.getNumColumns()), false);
+            MatrixBlock blkOutCorr = new MatrixBlock(Math.max(blkInA.getNumRows(), blkInB.getNumRows()), Math.max(blkInA.getNumColumns(), blkInB.getNumColumns()) + 1, false);
+
+            for (int j=0; j<smallerC; ++j) {
+                int nA = (int)blkInCorrA.getValue(blkInCorrB.getNumRows() - 1, j);
+                int nB = (int)blkInCorrB.getValue(blkInCorrB.getNumRows() - 1, j);
+
+                MatrixBlock shorterMatrix = blkInA;
+                MatrixBlock shorterMatrixCorr = blkInCorrA;
+                int smallerR = blkInA.getNumRows();
+                int largerR = blkInB.getNumRows();
+                if (nB < nA) {
+                    shorterMatrix = blkInB;
+                    shorterMatrixCorr = blkInCorrB;
+                    smallerR = blkInB.getNumRows();
+                    largerR = blkInA.getNumRows();
+                }
+
+                for (int i=0; i<smallerR; ++i) {
+                    int k = (int)blkInA.getValue(i, j);
+                    int v = (int)blkInCorrA.getValue(i, j);
+                    hashBuckets.put(k, v);
+
+                    k = (int)blkInB.getValue(i, j);
+                    v = (int)blkInCorrB.getValue(i, j);
+                    hashBuckets.put(k, v);
+                }
+
+                // Todo fix bug: need taller matrix here instead of shorter
+                for (int i=smallerR; i<largerR; ++i) {
+                    int k = (int)shorterMatrix.getValue(i, j);
+                    int v = (int)shorterMatrixCorr.getValue(i, j);
+                    hashBuckets.put(k, v);
+                }
+
+                int i=0;
+                for (Pair<Integer, Integer> kvPair : hashBuckets) {
+                    int k = kvPair.getKey();
+                    int v = kvPair.getValue();
+                    blkOut.setValue(i, j, k);
+                    blkOutCorr.setValue(i, j, v);
+                }
+
+                blkOutCorr.setValue(blkOutCorr.getNumRows() - 1, j, hashBuckets.size());
+
+                hashBuckets.clear();
+            }
+
+            for (int j=smallerC; j<largerC; ++j) {
+                int nHashes = (int)widerMatrix.getValue(widerMatrix.getNumRows() - 1, j);
+                for (int i=0; i<nHashes; ++i) {
+                    int k = (int)widerMatrix.getValue(i, j);
+                    int v = (int)widerMatrixCorr.getValue(i, j);
+
+                    blkOut.setValue(i, j, k);
+                    blkOutCorr.setValue(i, j, v);
+                }
+
+                blkOutCorr.setValue(blkOutCorr.getNumRows() - 1, j, nHashes);
+            }
+
+            return new CorrMatrixBlock(blkOut, blkOutCorr);
 
         } else {
             throw new IllegalArgumentException("Invalid direction");
         }
-
-        throw new NotImplementedException("union has not been implemented yet");
     }
 
     @Override
